577a7aa31e34ac4ac6a61d86d0d9143c1a7b977d
Rollphes <hiroto2054.314@gmail.com>
2021-03-24 00:01:43 +0900

    ミス治した

--- commands/game/omikuji.js
+++ commands/game/omikuji.js
@@ -1,39 +1,38 @@
 const {
-    rnd
-  } = require("../../functions.js");
+  rnd
+} = require("../../functions.js");
 module.exports = {
- name: "omikuji",
- category: "game",
- description: "おみくじコマンドです。一日一回までですよ？",
- abb: "omi",
-     run: (client, message, args) => {
-     swich(rnd)
-     {
-         case 1:
-             message.channel.send("大凶ですよ.....。運の使い過ぎには気を付けてください....。");
-             break;
-         case 2:
-             message.channel.send("凶ですね。よくないことが起きちゃうかもしれません。調子の乗りすぎには気を付けて....");
-             break;
-         case 3:
-             message.channel.send('末吉です。もうちょっと努力をしましょうね');
-             break;
-         case 4:
-             message.channel.send("中吉です。すこしばかりいいことが起きるかもしれませんね。");
-             break;
-         case 5:
-             message.channel.send("小吉です。ん～。何とも言えませんね。");
-             brake;
-         case 6:
-             message.channel.send("吉です。いいことが起きるかもしれません。いい努力を続けましょう");
-             brake;
-         case 7;
-             message.channel.send("大吉です。おめでとうございます。きっといいことが起きますよ。");
-             brake;
+  name: "omikuji",
+  category: "game",
+  description: "おみくじコマンドです。一日一回までですよ？",
+  abb: "omi",
+  run: (client, message, args) => {
+    switch (rnd) {
+      case 1:
+        message.channel.send("大凶ですよ.....。運の使い過ぎには気を付けてください....。");
+        break;
+      case 2:
+        message.channel.send("凶ですね。よくないことが起きちゃうかもしれません。調子の乗りすぎには気を付けて....");
+        break;
+      case 3:
+        message.channel.send('末吉です。もうちょっと努力をしましょうね');
+        break;
+      case 4:
+        message.channel.send("中吉です。すこしばかりいいことが起きるかもしれませんね。");
+        break;
+      case 5:
+        message.channel.send("小吉です。ん～。何とも言えませんね。");
+        break;
+      case 6:
+        message.channel.send("吉です。いいことが起きるかもしれません。いい努力を続けましょう");
+        break;
+      case 7:
+        message.channel.send("大吉です。おめでとうございます。きっといいことが起きますよ。");
+        break;
 
-             //エラー表示がほとんど出てます。
- 
- 
-        }
-     }
- }
+      //エラー表示がほとんど出てます。
+
+
+    }
+  }
+}
