cfd008d60f07b2f29555b47858e2789fac6acfa0
akikaki <ryofuruhashi823@gmail.com>
2021-03-23 21:17:43 +0900

    omikuji.js add

--- commands/game/omikuji.js
+++ commands/game/omikuji.js
@@ -1,18 +1,39 @@
-const { Message } = require("discord.js");
-
+const {
+    rnd
+  } = require("../../functions.js");
 module.exports = {
  name: "omikuji",
  category: "game",
  description: "おみくじコマンドです。一日一回までですよ？",
  abb: "omi",
      run: (client, message, args) => {
-    var omikuji = Math.floor( Math.random() * (7 + 1 - 1) ) + 1 ;
-    if ( omikuji == 1 ) { message.channel.send("大凶ですよ.....。運の使い過ぎには気を付けてください....。")}
-    if ( omikuji == 2 ) { message.channel.send("凶ですね。よくないことが起きちゃうかもしれません。調子の乗りすぎには気を付けて....")}
-    if ( omikuji == 3 ) { message.channel.send("末吉です。もうちょっと努力をしましょうね。")}
-    if ( omikuji == 4 ) { message.channel.send("中吉です。すこしばかりいいことが起きるかもしれませんね。")}
-    if ( omikuji == 5 ) { message.channel.send("小吉です。ん～。何とも言えませんね。")}
-    if ( omikuji == 6 ) { message.channel.send("吉です。いいことが起きるかもしれません。いい努力を続けましょう。")}
-    if ( omikuji == 7 ) { message.channel.send("大吉です。おめでとうございます。きっといいことが起きますよ。")}
+     swich(rnd)
+     {
+         case 1:
+             message.channel.send("大凶ですよ.....。運の使い過ぎには気を付けてください....。");
+             break;
+         case 2:
+             message.channel.send("凶ですね。よくないことが起きちゃうかもしれません。調子の乗りすぎには気を付けて....");
+             break;
+         case 3:
+             message.channel.send('末吉です。もうちょっと努力をしましょうね');
+             break;
+         case 4:
+             message.channel.send("中吉です。すこしばかりいいことが起きるかもしれませんね。");
+             break;
+         case 5:
+             message.channel.send("小吉です。ん～。何とも言えませんね。");
+             brake;
+         case 6:
+             message.channel.send("吉です。いいことが起きるかもしれません。いい努力を続けましょう");
+             brake;
+         case 7;
+             message.channel.send("大吉です。おめでとうございます。きっといいことが起きますよ。");
+             brake;
+
+             //エラー表示がほとんど出てます。
+ 
+ 
+        }
      }
  }
