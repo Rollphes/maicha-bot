90f30ba15b021195ddd8fcb8e89cc5361fd0aaee
Rollphes <hiroto2054.314@gmail.com>
2021-05-03 11:36:19 +0900

    utf-8

--- commands/minecraft-addon-support/m-b.js
+++ commands/minecraft-addon-support/m-b.js
@@ -1,18 +1,24 @@
-const { uuid } = require("../../functions.js");
+const {
+  uuid
+} = require("../../functions.js");
 const fs = require("fs");
 module.exports = {
-    name: "m-b",
-    category: "minecraft-addon-support",
-    description: "behavior用のmanifestを作成します。m!m-b `name` `description` という感じで引数を指定できます。引数の指定がない場合undefinedが代入されます。",
-    run: (client, message, args) => {
-        if (args[1] == null) {
-            args[1] = "undefined";
-        }
-        if (args[0] == null) {
-            args[0] = "undefined";
-        }
-        fs.writeFileSync(`manifest.json`, `{\n    "format_version": 1,\n    "header": {\n        "description": "${args[1]}",\n        "name": "${args[0]}",\n        "uuid": "${uuid()}",\n     "min_engine_version": [1, 2, 6], \n         "version": [0, 0, 1]\n            },\n    "modules": [{\n        "description": "${args[1]}",\n        "type": "date",\n        "uuid": "${uuid()}",\n        "version": [0, 0, 1]\n    }]\n}`, 'utf8');
-        message.channel.send(`behavior`, { files: [`manifest.json`] })
-            .then(() => { fs.unlinkSync(`manifest.json`); });
+  name: "m-b",
+  category: "minecraft-addon-support",
+  description: "behavior用のmanifestを作成します。m!m-b `name` `description` という感じで引数を指定できます。引数の指定がない場合undefinedが代入されます。",
+  run: (client, message, args) => {
+    if (args[1] == null) {
+      args[1] = "undefined";
     }
+    if (args[0] == null) {
+      args[0] = "undefined";
+    }
+    fs.writeFileSync(`manifest.json`, `{\n    "format_version": 1,\n    "header": {\n        "description": "${args[1]}",\n        "name": "${args[0]}",\n        "uuid": "${uuid()}",\n     "min_engine_version": [1, 2, 6], \n         "version": [0, 0, 1]\n            },\n    "modules": [{\n        "description": "${args[1]}",\n        "type": "date",\n        "uuid": "${uuid()}",\n        "version": [0, 0, 1]\n    }]\n}`, 'utf-8');
+    message.channel.send(`behavior`, {
+      files: [`manifest.json`]
+    })
+    .then(() => {
+      fs.unlinkSync(`manifest.json`);
+    });
+  }
 }
--- commands/minecraft-addon-support/m-r.js
+++ commands/minecraft-addon-support/m-r.js
@@ -1,18 +1,24 @@
-const { uuid } = require("../../functions.js");
+const {
+  uuid
+} = require("../../functions.js");
 const fs = require("fs");
 module.exports = {
-    name: "m-r",
-    category: "minecraft-addon-support",
-    description: "resourceのmanifestを作成します。m!m-r `name` `description` という感じで引数を指定できます。引数の指定がない場合undefinedが代入されます。",
-    run: (client, message, args) => {
-        if (args[1] == null) {
-            args[1] = "undefined";
-        }
-        if (args[0] == null) {
-            args[0] = "undefined";
-        }
-        fs.writeFileSync(`manifest.json`, `{\n    "format_version": 1,\n    "header": {\n        "description": "${args[1]}",\n        "name": "${args[0]}",\n        "uuid": "${uuid()}",\n   "min_engine_version": [1, 2, 6], \n     "version": [0, 0, 1]\n    },\n    "modules": [{\n        "description": "${args[1]}",\n        "type": "resources",\n        "uuid": "${uuid()}",\n        "version": [0, 0, 1]\n    }]\n}`, 'utf8');
-        message.channel.send(`resource`, { files: [`manifest.json`] })
-            .then(() => { fs.unlinkSync(`manifest.json`); });
+  name: "m-r",
+  category: "minecraft-addon-support",
+  description: "resourceのmanifestを作成します。m!m-r `name` `description` という感じで引数を指定できます。引数の指定がない場合undefinedが代入されます。",
+  run: (client, message, args) => {
+    if (args[1] == null) {
+      args[1] = "undefined";
     }
+    if (args[0] == null) {
+      args[0] = "undefined";
+    }
+    fs.writeFileSync(`manifest.json`, `{\n    "format_version": 1,\n    "header": {\n        "description": "${args[1]}",\n        "name": "${args[0]}",\n        "uuid": "${uuid()}",\n   "min_engine_version": [1, 2, 6], \n     "version": [0, 0, 1]\n    },\n    "modules": [{\n        "description": "${args[1]}",\n        "type": "resources",\n        "uuid": "${uuid()}",\n        "version": [0, 0, 1]\n    }]\n}`, 'utf-8');
+    message.channel.send(`resource`, {
+      files: [`manifest.json`]
+    })
+    .then(() => {
+      fs.unlinkSync(`manifest.json`);
+    });
+  }
 }
